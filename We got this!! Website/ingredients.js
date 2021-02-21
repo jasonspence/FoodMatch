793aea476820079b55de879b7cce4faf52134890
Jason S <jason.spence123@outlook.com>
2021-02-21 05:05:17 -0800

    did we save it?

--- We got this!! Website/ingredients.js
+++ We got this!! Website/ingredients.js
@@ -33,6 +33,8 @@ const data = fetch(jsonPath)
 .then(console.log("Hey did this work??"))
 .then(data => console.log(data));
 
+console.log(data)
+
 for(const recipe in data.Cookbook) {
     console.log(recipe.Name)
 }
